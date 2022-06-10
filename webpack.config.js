4c0c211d6c3e8cf9ef5d6315939e1b665aea46d3
brndnpndy <pandaybg@gmail.com>
2022-06-10 10:40:48 -0400

    Change application name

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
   devtool: 'inline-source-map',
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'Output Management',
+      title: 'Do!t.app',
     }),
   ],
   output: {
